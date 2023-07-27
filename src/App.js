856f8e3d2c409081c42443c7c393b0888ff90bab
jovanatrajcheska <trajceskaj55@gmail.com>
2023-07-27 14:27:13 +0200

    App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Game.css';
+import ColorMatch from './screens/ColorMatch'; // Import the ColorMatch component
+
+function App() {
+  return (
+    <div className="App">
+      <ColorMatch /> {/* Render the ColorMatch component */}
+    </div>
+  );
+}
+
+export default App;
