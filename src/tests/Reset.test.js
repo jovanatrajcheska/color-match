9429a13d08cf71c30bb1ed843efa2538a3dbd2c7
jovanatrajcheska <trajceskaj55@gmail.com>
2023-07-31 12:42:44 +0200

    Changes for Reset

--- src/tests/Reset.test.js
+++ src/tests/Reset.test.js
@@ -3,21 +3,21 @@ import { resetGameAfterDelay } from '../helpers/Reset';
 
 jest.useFakeTimers();
 
+const setColors = jest.fn();
+const setGameStatus = jest.fn();
+
 test('testing Reset', () => {
-  const setColors = jest.fn();
-  const setGameStatus = jest.fn();
 
-  resetGameAfterDelay(setColors, setGameStatus);
+    resetGameAfterDelay(setColors, setGameStatus);
 
-  expect(setColors).not.toHaveBeenCalled();
-  expect(setGameStatus).not.toHaveBeenCalled();
+    expect(setColors).not.toHaveBeenCalled();
+    expect(setGameStatus).not.toHaveBeenCalled();
 
-  act(() => {
-    jest.advanceTimersByTime(1000);
-  });
+    act(() => {
+        jest.advanceTimersByTime(1000);
+    });
 
-
-  expect(setColors).toHaveBeenCalledTimes(1);
-  expect(setGameStatus).toHaveBeenCalledTimes(1);
-  expect(setGameStatus).toHaveBeenCalledWith('playing');
+    expect(setColors).toHaveBeenCalledTimes(1);
+    expect(setGameStatus).toHaveBeenCalledTimes(1);
+    expect(setGameStatus).toHaveBeenCalledWith('playing');
 });
