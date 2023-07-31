d8fc44750ff01a1f4775b3ef131346b00b29a719
jovanatrajcheska <trajceskaj55@gmail.com>
2023-07-31 09:15:50 +0200

    Changes for Question component

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,21 +1,9 @@
 import React from 'react';
-import { getRandomColors } from '../helpers/RandomColors';
 
-const Question = () => {
-
-  const isTopWord = Math.random() >= 0.5;
-  const colors = getRandomColors();
-  const { wordOne, wordTwo, inkColor } = colors;
-  const isMatch = wordOne === inkColor;
-
-  
+const QuestionText = ({ question }) => {
   return (
-    <div className="question">
-      Does the meaning of the{' '}
-      <b>{isTopWord ? 'top' : 'bottom'}</b> word match the ink color of the{' '}
-      <b>{isTopWord ? 'bottom' : 'top'}</b> word?
-    </div>
+    <div className="question">{question}</div>
   );
 };
 
-export default Question;
+export default QuestionText;
