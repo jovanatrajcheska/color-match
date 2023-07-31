ac19de3a0a73e7a7ca58a19f03855d93690cee89
jovanatrajcheska <trajceskaj55@gmail.com>
2023-07-31 10:49:36 +0200

    Changes for ColorDisplay

--- src/components/ColorDisplay.js
+++ src/components/ColorDisplay.js
@@ -1,27 +1,27 @@
 import React from 'react';
 
 const ColorDisplay = ({ wordOne, wordTwo, inkColor, gameStatus }) => {
-  const toggleColorPosition = Math.random() >= 0.5;
-  const displayWordOne = toggleColorPosition ? wordOne : wordTwo;
-  const displayWordTwo = toggleColorPosition ? wordTwo : wordOne;
+    const toggleColorPosition = Math.random() >= 0.5;
+    const displayWordOne = toggleColorPosition ? wordOne : wordTwo;
+    const displayWordTwo = toggleColorPosition ? wordTwo : wordOne;
 
-  return (
-    <div className="body">
-      <div className="words-container">
-        <div className="ink" style={{ color: inkColor }}>
-          {displayWordOne}
+    return (
+        <div className="body">
+            <div className="words-container">
+                <div className="ink" style={{ color: inkColor }} data-ink-color={inkColor}>
+                    {displayWordOne}
+                </div>
+                <div className="meaning">
+                    {displayWordTwo}
+                </div>
+            </div>
+            {gameStatus !== 'playing' && (
+                <div className={`game-status status-${gameStatus}`}>
+                    {gameStatus === 'correct' ? '✓' : '✗'}
+                </div>
+            )}
         </div>
-        <div className="meaning">
-          {displayWordTwo}
-        </div>
-      </div>
-      {gameStatus !== 'playing' && (
-        <div className={`game-status status-${gameStatus}`}>
-          {gameStatus === 'correct' ? '✓' : '✗'}
-        </div>
-      )}
-    </div>
-  );
+    );
 };
 
 export default ColorDisplay;
