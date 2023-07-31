9ee6db0dff85b226a05a1ab56d8db553e7ad0883
jovanatrajcheska <trajceskaj55@gmail.com>
2023-07-31 10:40:18 +0200

    Tests for ColorGenerator

--- src/tests/ColorGenerator.test.js
+++ src/tests/ColorGenerator.test.js
@@ -6,33 +6,30 @@ const mockMathRandom = (value) => {
     const originalMathRandom = global.Math.random;
     global.Math.random = () => value;
     return () => {
-      global.Math.random = originalMathRandom;
+        global.Math.random = originalMathRandom;
     };
-  };
-  
-test('should generate random colors for wordOne and wordTwo', () => {
-  const randomG = jest.spyOn(Math, 'random').mockReturnValue(0.5);
+};
 
-  const colors = getRandomColors();
-
-  expect(colors.wordOne).toMatch(/black|blue|red|green|yellow|pink|purple|cyan/);
-  expect(colors.wordTwo).toMatch(/black|blue|red|green|yellow|pink|purple|cyan/);
-
-
-  jest.advanceTimersByTime(1000);
-
-
-  randomG.mockRestore();
-  jest.clearAllTimers();
-});
-test('should generate random inkColor', () => {
-    const randomValue = 0.4; // Set to produce inkColor as wordOne
-    const restoreMathRandom = mockMathRandom(randomValue);
-  
+test('random colors for wordOne and wordTwo', () => {
+    const randomG = jest.spyOn(Math, 'random').mockReturnValue(0.5);
     const colors = getRandomColors();
-  
+
+    expect(colors.wordOne).toMatch(/black|blue|red|green|yellow|pink|purple|cyan/);
+    expect(colors.wordTwo).toMatch(/black|blue|red|green|yellow|pink|purple|cyan/);
+
+    jest.advanceTimersByTime(1000);
+
+    randomG.mockRestore();
+    jest.clearAllTimers();
+});
+
+test('random inkColor', () => {
+    const randomValue = 0.4;
+    const restoreMathRandom = mockMathRandom(randomValue);
+
+    const colors = getRandomColors();
+
     expect(colors.inkColor).toBe(colors.wordOne);
-  
+
     restoreMathRandom();
-  });
-  
+});
