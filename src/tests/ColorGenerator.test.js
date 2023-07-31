2d28b2476e50b6ae9a04864f239ff48e301f9a1e
jovanatrajcheska <trajceskaj55@gmail.com>
2023-07-31 10:32:34 +0200

    Tests for ColorGenerator

---
+++ src/tests/ColorGenerator.test.js
@@ -0,0 +1,38 @@
+import { getRandomColors } from '../helpers/ColorGenerator';
+
+jest.useFakeTimers();
+
+const mockMathRandom = (value) => {
+    const originalMathRandom = global.Math.random;
+    global.Math.random = () => value;
+    return () => {
+      global.Math.random = originalMathRandom;
+    };
+  };
+  
+test('should generate random colors for wordOne and wordTwo', () => {
+  const randomG = jest.spyOn(Math, 'random').mockReturnValue(0.5);
+
+  const colors = getRandomColors();
+
+  expect(colors.wordOne).toMatch(/black|blue|red|green|yellow|pink|purple|cyan/);
+  expect(colors.wordTwo).toMatch(/black|blue|red|green|yellow|pink|purple|cyan/);
+
+
+  jest.advanceTimersByTime(1000);
+
+
+  randomG.mockRestore();
+  jest.clearAllTimers();
+});
+test('should generate random inkColor', () => {
+    const randomValue = 0.4; // Set to produce inkColor as wordOne
+    const restoreMathRandom = mockMathRandom(randomValue);
+  
+    const colors = getRandomColors();
+  
+    expect(colors.inkColor).toBe(colors.wordOne);
+  
+    restoreMathRandom();
+  });
+  
