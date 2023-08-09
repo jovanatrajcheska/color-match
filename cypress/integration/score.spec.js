8d03db57346a1b6c35c772c9497e8ff83ff10ff8
jovanatrajcheska <trajceskaj55@gmail.com>
2023-08-09 15:19:16 +0200

    Add tests for score

--- cypress/integration/score.spec.js
+++ cypress/integration/score.spec.js
@@ -7,31 +7,25 @@ describe("Score", () => {
     cy.get(".score").should("have.text", "Score: 0");
   });
 
- 
-  it('should correctly update the score based on the game logic', () => {
+  it("should correctly update the score based on the game logic", () => {
     cy.window().then((win) => {
       const isMatch = Math.random() < 0.5;
       win.Math.random = () => (isMatch ? 0.4 : 0.6);
     });
-  
-  
+
     cy.window().then((win) => {
       const isMatch = Math.random() < 0.5;
-      const buttonToClick = isMatch ? 'YES' : 'NO';
-  
+      const buttonToClick = isMatch ? "YES" : "NO";
+
       // click on button
       cy.contains(buttonToClick).click();
-  
+
       // update the score
       if (isMatch) {
-        cy.get('.score').should('have.text', 'Score: 1');
+        cy.get(".score").should("have.text", "Score: 1");
       } else {
-        cy.get('.score').should('have.text', 'Score: -1');
+        cy.get(".score").should("have.text", "Score: -1");
       }
     });
   });
-  
-  
-
-  
 });
